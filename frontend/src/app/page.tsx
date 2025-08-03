5e8ada067d93a70541d67b15aa0cae78d2a79f7a
EomUhyeon <righthyeon00@gmail.com>
2025-08-03 22:24:43 +0900

    frontend 기본 구성 중...

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '2rem' }}>
+      <h1>Hello World test</h1>
+    </main>
+  );
+}
